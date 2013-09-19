f1c2be25d3460a3a96710e898e6470d23e1161fa
jmoline <jmoline@practicefusion.com>
2013-09-19 15:01:09 -0700

    adding dist folder for deployment to gh-pages

---
+++ dist/scripts/899dac60.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("ehrApp",[]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("ehrApp").controller("MainCtrl",["$scope",function(){}]);
